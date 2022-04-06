2afda9ba29f0bf55b6e8f5422d7524399f052aec
Afonne-CID <delightinbusiness@gmail.com>
2022-04-06 04:45:41 -0700

    refactored

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,29 +8,13 @@ def pascal_triangle(n):
     """ Returns a list of lists of integers representing the
         Pascal's triangle of `n`
     """
-    if (n <= 0):
-        return []
-    if (n == 1):
-        return [[1]]
-    if (n == 2):
-        return [[1], [1, 1]]
-
-    my_list = [[1], [1, 1]]
-
-    while ((n - 2) > 0):
-        length = len(my_list[-1]) - 1
-        tmp_length = length
-        to_append = []
-
-        while (tmp_length > 0):
-            if (my_list[-1][(length - tmp_length) + 1]):
-                to_append.append(my_list[-1][length - tmp_length]
-                                 + my_list[-1][(length - tmp_length) + 1])
-            tmp_length -= 1
-        to_append.append(1)
-        to_append.insert(0, 1)
-        my_list.append(to_append)
-
-        n -= 1
-
+    my_list = []
+    if (n > 0):
+        for i in range(1, n + 1):
+            inner = []
+            cnt = 1
+            for j in range(1, i + 1):
+                inner.append(cnt)
+                cnt = cnt * (i - j) // j
+            my_list.append(inner)
     return my_list
