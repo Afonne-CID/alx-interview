e27e3c3b269861c21b4c3e06c865b818c21768d8
Afonne-CID <delightinbusiness@gmail.com>
2022-05-04 00:21:35 -0700

    update

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,9 +8,12 @@ def validUTF8(data: list) -> bool:
     '''Checks if a given data set is a valid UTF-8
     '''
     for num in data:
-        base = 128
-        if (num & base):
-            return False
+        if type(num) == int:
+            base = 128
+            if num & base:
+                return False
+            else:
+                continue
         else:
-            continue
+            return False
     return True
