81d959258c9c5feb801b918e8e199d9004b5eb91
Afonne-CID <delightinbusiness@gmail.com>
2022-05-04 00:44:14 -0700

    val

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,13 +7,21 @@ a valid UTF-8 ecnoding
 def validUTF8(data: list) -> bool:
     '''Checks if a given data set is a valid UTF-8
     '''
+    cnt = 0
     for num in data:
         if type(num) == int:
-            base = 128
-            if num & base:
-                return False
+            if not cnt:
+                base = 128
+                while base & num:
+                    cnt += 1
+                    base >>= 1
+                if not cnt:
+                    continue
+                if cnt == 1 or cnt > 4:
+                    return False
             else:
-                continue
+                if num >> 6 != 0b10:
+                    return False
         else:
             return False
-    return True
+    return cnt == 0
