0a24134f3e8847ac4c706a4678bd0300221c207e
Afonne-CID Paul Onyedikachi <75165644+Afonne-CID@users.noreply.github.com>
2022-06-17 02:23:19 +0100

    Fix complete

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -10,23 +10,23 @@ def isWinner(x, nums):
     if not nums or x < 1:
         return None
     n = max(nums)
-    fltr = [True for _ in range(max(n + 1, 2))]
+    filter = [True for _ in range(max(n + 1, 2))]
     for i in range(2, int(pow(n, 0.5)) + 1):
-        if not fltr[i]:
+        if not filter[i]:
             continue
         for j in range(i * i, n + 1, i):
-            fltr[j] = False
-    fltr[0] = fltr[1] = False
+            filter[j] = False
+    filter[0] = filter[1] = False
     c = 0
-    for i in range(len(fltr)):
-        if fltr[i]:
+    for i in range(len(filter)):
+        if filter[i]:
             c += 1
-        fltr[i] = c
-    plyr1 = 0
+        filter[i] = c
+    pl1 = 0
     for n in nums:
-        plyr1 += fltr[n] % 2 == 1
-    if plyr1 * 2 == len(nums):
+        pl1 += filter[n] % 2 == 1
+    if pl1 * 2 == len(nums):
         return None
-    if plyr1 * 2 > len(nums):
+    if pl1 * 2 > len(nums):
         return "Maria"
     return "Ben"
