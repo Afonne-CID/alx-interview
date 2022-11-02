3b732140e86da93702511427627fdb90797d0b6b
afonne-cid <delightinbusiness@gmail.com>
2022-11-02 05:53:17 +0100

    update: new algo

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,13 +8,25 @@ def pascal_triangle(n):
     """ Returns a list of lists of integers representing the
         Pascal's triangle of `n`
     """
+
+    # if n <= 0:
+    #     return []
+    # res = [[1]]
+    # for _ in range(n - 1):
+    #     tmp = [0] + res[-1] + [0]
+    #     row = []
+    #     for i in range(len(tmp) - 1):
+    #         row.append(tmp[i] + tmp[i+1])
+    #     res.append(row)
+    # return res
+
     my_list = []
     if (n > 0):
         for i in range(1, n + 1):
-            inner = []
+            row = []
             cnt = 1
             for j in range(1, i + 1):
-                inner.append(cnt)
+                row.append(cnt)
                 cnt = cnt * (i - j) // j
-            my_list.append(inner)
+            my_list.append(row)
     return my_list
